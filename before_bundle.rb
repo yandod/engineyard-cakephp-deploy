06c78ecc33a6a5fc66e17db2964f0beac76eef68
Yusuke Ando <yandod@gmail.com>
2013-03-23 17:53:29 +0900

    up

--- before_bundle.rb
+++ before_bundle.rb
@@ -1,4 +1,5 @@
 # prepare shared files and directories
+run "rm #{release_path}/app/Config/core.php && ln -s #{shared_path}/app/Config/core.php #{release_path}/app/Config/core.php"
 run "ln -s #{shared_path}/app/Config/database.php #{release_path}/app/Config/database.php"
 run "ln -s #{shared_path}/app/Plugin #{release_path}/app/Plugin"
 run "ln -s #{shared_path}/app/files #{release_path}/app/files"
@@ -19,4 +20,12 @@ sudo "echo 'allow_url_fopen = On' > /etc/php/fpm-php5.3/ext-active/timezone.ini"
 
 # change nginx document root for CakePHP
 run 'sed  -e \'s/root \/data\/' + app  + '\/current;/root \/data\/' + app  + '\/current\/app\/webroot;/\' /etc/nginx/servers/' + app  + '.conf > /etc/nginx/servers/' + app  + '.conf.tmp'
+
+# TODO: this piece has to be injected somehow.
+# if (!-e $request_filename) {
+#     rewrite ^(.+)$  /index.php?url=$1 last;
+#     break;
+# }
+            
+
 run "cp /etc/nginx/servers/" + app  + ".conf.tmp /etc/nginx/servers/" + app  + ".conf"
