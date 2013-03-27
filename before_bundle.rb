c2c503fef06a9a58903c26ea8e6e8a5439aa45e3
Yusuke Ando <yandod@gmail.com>
2013-03-27 15:01:57 +0900

    fix sed

--- before_bundle.rb
+++ before_bundle.rb
@@ -5,21 +5,27 @@ run "ln -s #{shared_path}/app/Plugin #{release_path}/app/Plugin"
 run "ln -s #{shared_path}/app/files #{release_path}/app/files"
 
 # set timezone in php.ini
-sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/cgi-php5.3/ext-active/timezone.ini"
-sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/cli-php5.3/ext-active/timezone.ini"
-sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/fpm-php5.3/ext-active/timezone.ini"
+sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/cgi-php5.4/ext-active/timezone.ini"
+sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/cli-php5.4/ext-active/timezone.ini"
+sudo "echo 'date.timezone = Asia/Tokyo' > /etc/php/fpm-php5.4/ext-active/timezone.ini"
 
 # set allow_url_fopen = On
-sudo "echo 'allow_url_fopen = On' > /etc/php/cgi-php5.3/ext-active/allow_url.ini"
-sudo "echo 'allow_url_fopen = On' > /etc/php/cli-php5.3/ext-active/allow_url.ini"
-sudo "echo 'allow_url_fopen = On' > /etc/php/fpm-php5.3/ext-active/allow_url.ini"
+sudo "echo 'allow_url_fopen = On' > /etc/php/cgi-php5.4/ext-active/allow_url.ini"
+sudo "echo 'allow_url_fopen = On' > /etc/php/cli-php5.4/ext-active/allow_url.ini"
+sudo "echo 'allow_url_fopen = On' > /etc/php/fpm-php5.4/ext-active/allow_url.ini"
+
+# set APC
+#sudo "echo 'extension=apc.so' > /etc/php/cgi-php5.4/ext-active/apc.ini"
+#sudo "echo 'extension=apc.so' > /etc/php/cli-php5.4/ext-active/apc.ini"
+#sudo "echo 'extension=apc.so' > /etc/php/fpm-php5.4/ext-active/apc.ini"
+
 
 # kick composer install
 #run "curl -s https://getcomposer.org/installer | php -d allow_url_fopen=on"
 #run "php -d allow_url_fopen=on composer.phar install"
 
 # change nginx document root for CakePHP
-run 'sed  -e \'s/root \/data\/' + app  + '\/current;/root \/data\/' + app  + '\/current\/app\/webroot;/\' /etc/nginx/servers/' + app  + '.conf > /etc/nginx/servers/' + app  + '.conf.tmp'
+run 'sed  -e \'s/\/data\/' + app  + '\/current;/\/data\/' + app  + '\/current\/app\/webroot;/\' /etc/nginx/servers/' + app  + '.conf > /etc/nginx/servers/' + app  + '.conf.tmp'
 
 # TODO: this piece has to be injected somehow.
 # if (!-e $request_filename) {
