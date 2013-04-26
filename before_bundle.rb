bcda02e7c412fb229774c19bb89a9b4b915701ef
Yusuke Ando <yandod@gmail.com>
2013-04-26 12:11:15 +0900

    update

--- before_bundle.rb
+++ before_bundle.rb
@@ -1,3 +1,25 @@
+if !Dir.exist?(shared_path + "/app/Config") then
+  run "mkdir -p #{shared_path}/app/Config/"
+  run "cp #{release_path}/app/Config/core.php #{shared_path}/app/Config/core.php"
+  run "echo \"<?php
+  class DATABASE_CONFIG {
+  	var \$default = array(
+  		'datasource' => 'Database/Mysql',
+  		'persistent' => false,
+  		'host' => \$_SERVER[\"DB_HOST\"],
+  		'login' => \$_SERVER[\"DB_USER\"],
+  		'password' => \$_SERVER[\"DB_PASS\"],
+  		'database' => \$_SERVER[\"DB_NAME\"],
+  		'prefix' => '',
+  		'port' => '',
+  		'encoding' => 'utf8',
+  	);
+  }\" > #{shared_path}/app/Config/database.php"
+  run "mkdir -p #{shared_path}/app/files/"
+  run "mkdir -p #{shared_path}/app/Plugin/"
+  run "mysql #{app} < #{release_path}/app/Config/sql/mysql.sql"
+end
+
 # prepare shared files and directories
 run "rm #{release_path}/app/Config/core.php && ln -s #{shared_path}/app/Config/core.php #{release_path}/app/Config/core.php"
 run "ln -s #{shared_path}/app/Config/database.php #{release_path}/app/Config/database.php"
@@ -25,13 +47,10 @@ sudo "echo 'allow_url_fopen = On' > /etc/php/fpm-php5.4/ext-active/allow_url.ini
 #run "php -d allow_url_fopen=on composer.phar install"
 
 # change nginx document root for CakePHP
-run 'sed  -e \'s/\/data\/' + app  + '\/current\/;/\/data\/' + app  + '\/current\/app\/webroot;/\' /etc/nginx/servers/' + app  + '.conf > /etc/nginx/servers/' + app  + '.conf.tmp'
+#run 'sed  -e \'s/\/data\/' + app  + '\/current\/;/\/data\/' + app  + '\/current\/app\/webroot;/\' /etc/nginx/servers/' + app  + '.conf > /etc/nginx/servers/' + app  + '.conf.tmp'
 
 # TODO: this piece has to be injected somehow.
-# if (!-e $request_filename) {
-#     rewrite ^(.+)$  /index.php?url=$1 last;
-#     break;
-# }
+# @proxy;\n
+# @proxy ; if (!-e $request_filename) { rewrite ^(.+)$  /index.php?url=$1 last; break; }
             
-
-run "cp /etc/nginx/servers/" + app  + ".conf.tmp /etc/nginx/servers/" + app  + ".conf"
+# run "cp /etc/nginx/servers/" + app  + ".conf.tmp /etc/nginx/servers/" + app  + ".conf"
